817816257d95ce2c6b6f39322f855980e1be03cf
Dave Jensen <david@jensen47.com>
2018-11-16 04:58:04 +0000

    feat: replace link url if post was imported

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ var winston = module.parent.require('winston');
 var FixPath = {};
 
 const TAG = '[plugins/fix-relative-path] ';
+const verbose = (msg) => {
+  winston.verbose(TAG+msg);
+}
 
 FixPath.parsePost = (data, callback) => {
-  winston.verbose(TAG + JSON.stringify(data));
-  callback(null)
+  const post = data.postData;
+  if (!!post['__imported_original_data__']) {
+    post.content = post.content.replace(/((\[.*\])\((\/uploads\/.*)\))/gm, '$2(/forums/assets$3)');
+  }
+  callback(null, data);
 };
 
 module.exports = FixPath;
